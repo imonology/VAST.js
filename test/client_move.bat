db3b8f3c6c4d981dfa5c5e07c321c35bb1119597
Miguel Smith <19287887@sun.ac.za>
2018-08-27 16:36:55 +0200

    Created visualiser
    This is the first working version of the visualiser. Run visualiser.html and select the socket.io port of the client whose view you wish to see.

---
+++ test/client_move.bat
@@ -0,0 +1,1 @@
+node test_von_peer 127.0.0.1:37700 true;
--- test/test_VON_peer.js
+++ test/test_VON_peer.js
@@ -6,7 +6,7 @@
 
 
 // flags
-var AUTOMATIC_LEAVE_PERIOD = 3;     // number of seconds 
+var AUTOMATIC_LEAVE_PERIOD = 3;     // number of seconds
 
 require('../lib/common.js');
 
@@ -26,8 +26,10 @@ if (process.argv[2]) {
 		addr.host = VAST.Settings.IP_gateway;
 	else
 		is_client = true;
-		
+
 	gateway_addr = addr;
+
+	var move = process.argv[3];
 }
 
 LOG.debug('GW ip: ' + gateway_addr.host + ' port: ' + gateway_addr.port);
@@ -36,27 +38,38 @@ LOG.debug('is_client: ' + is_client);
 var x = Math.floor(Math.random() * 100);
 var y = Math.floor(Math.random() * 100);
 
+x = x == 0 ? x+1: x;
+y = y == 0 ? y+1: y;
+
 // create GW or a connecting client;
 var peer = new VON.peer();
-var aoi  = new VAST.area(new VAST.pos(x, y), 100);
-        
+var aoi  = new VAST.area(new VAST.pos(x, y), 10);
+
 var moveAround = function () {
-     
+
     // move if not GW
     if (peer.getSelf().id !== VAST.ID_GATEWAY) {
         // random walk new location (5 units within current center position)
-        aoi.center.x += Math.floor((Math.random()%10) - 5);
-        aoi.center.y += Math.floor((Math.random()%10) - 5);
-        
+        aoi.center.x += Math.floor((Math.random()*10) - 5);
+        aoi.center.y += Math.floor((Math.random()*10) - 5);
+
         if (aoi.center.x < 0)
             aoi.center.x *= -1;
+		if (aoi.center.x == 0)
+			aoi.center.x = 1;
+		if (aoi.center.x >= 100)
+			aoi.center.x = 99;
+		if (aoi.center.y >= 100)
+			aoi.center.y = 99;
+		if (aoi.center.y == 0)
+			aoi.center.y = 1;
         if (aoi.center.y < 0)
             aoi.center.y *= -1;
-            
+
     }
-    
+
     var neighbor_size = Object.keys(peer.list()).length;
-    console.log('move around to ' + aoi.center.toString() + ' neighbor size: ' + neighbor_size);    
+    console.log('move around to ' + aoi.center.toString() + ' neighbor size: ' + neighbor_size);
     peer.move(aoi);
 }
 
@@ -67,17 +80,16 @@ var interval_id = undefined;
 // after init the peer will bind to a local port
 peer.init((is_client ? VAST.ID_UNASSIGNED : VAST.ID_GATEWAY), gateway_addr.port, function () {
 
-    peer.join(gateway_addr, aoi, 
+    peer.join(gateway_addr, aoi,
 
         // done callback
         function (id) {
             LOG.warn('joined successfully! id: ' + id + '\n');
-            
-            // try to move around once in a while...  (if not gateway)        
-            if (id !== VAST.ID_GATEWAY) {
-                interval_id = setInterval(function(){ moveAround() }, 1000); 
-            }        
+
+            // try to move around once in a while...  (if not gateway)
+            if (id !== VAST.ID_GATEWAY && move == 'true') {
+                interval_id = setInterval(function(){ moveAround() }, 5000);
+            }
         }
     );
 });
-
