8ea056ea0d98b65f243656512565db9d29966b87
Miguel Smith <19287887@sun.ac.za>
2019-09-20 10:43:18 +0200

    Updated multiple matcher progress

--- test/vast_client_gateway.bat
+++ test/vast_client_gateway.bat
@@ -1,1 +1,1 @@
-node test_VAST_client.js false 10.10.11.179 37700 100 10.10.11.179 50 50 > C:\Users\"Miguel Smith"\Documents\"Varsity Work"\Masters\scripts\"Starter scripts"\Logs\gateway.txt
+node test_VAST_client.js false 10.10.11.179 37700 100 10.10.11.179 50 50 > C:\Users\"Miguel Smith"\Documents\"Varsity Work"\Masters\"Minecraft data parser"\Logs\gateway.txt
