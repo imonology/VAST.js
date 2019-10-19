ed145d613c52133490ce6cee3997068afa3fce75
Miguel Smith <19287887@sun.ac.za>
2019-10-19 14:52:15 +0200

    Bug fixes for matcher pub/sub functionality

--- test/vast_client.bat
+++ test/vast_client.bat
@@ -1,1 +1,1 @@
-node test_VAST_client.js true 10.10.11.179 37700 100 10.10.11.179 990 990 > C:\Users\"Miguel Smith"\Documents\"Varsity Work"\Masters\"Minecraft data parser"\Logs\client.txt
+node test_VAST_client.js true 10.10.11.179 37700 100 10.10.11.179 90 90 > C:\Users\"Miguel Smith"\Documents\"Varsity Work"\Masters\"Minecraft data parser"\Logs\client.txt
