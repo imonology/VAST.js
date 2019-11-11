dfbd0e31f38748c05527b6da5cb06a2700859f2b
Miguel Smith <19287887@sun.ac.za>
2019-11-11 08:56:17 +0200

    Backup (errors with "within" function)

--- test/vast_client2.bat
+++ test/vast_client2.bat
@@ -1,1 +1,1 @@
-node test_VAST_client.js true 10.10.11.179 37700 100 10.10.11.179 900 0 > C:\Users\"Miguel Smith"\Documents\"Varsity Work"\Masters\"Minecraft data parser"\Logs\clientMatch2.txt
+node test_VAST_client.js true 10.10.11.179 37700 100 10.10.11.179 500 500 > C:\Users\"Miguel Smith"\Documents\"Varsity Work"\Masters\"Minecraft data parser"\Logs\clientMatch2.txt
