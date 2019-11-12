723f41d49fa77b3e14df7fcdc0024233345e98e3
Miguel Smith <19287887@sun.ac.za>
2019-11-12 18:24:07 +0200

    Further bug fixing on load balancing
    Currently working on pending client connection debugging

--- test/vast_client.bat
+++ test/vast_client.bat
@@ -1,1 +1,1 @@
-node test_VAST_client.js true 10.10.11.179 37700 100 10.10.11.179 800 800 > C:\Users\"Miguel Smith"\Documents\"Varsity Work"\Masters\"Minecraft data parser"\Logs\client.txt
+node test_VAST_client.js true 10.110.117.14 37700 100 10.110.117.14 800 800 > C:\Users\"Miguel Smith"\Documents\"Varsity Work"\Masters\"Minecraft data parser"\Logs\client.txt
