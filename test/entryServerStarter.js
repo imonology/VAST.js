cd80fd59c90488334a770cdf3e6e37b9e19ff6b4
Miguel Smith <MiguelSmith1995@users.noreply.github.com>
2019-11-17 21:18:52 +0200

    Work on trying to get boundary maths right
    Also some adjustments for Minecraft
    
    
    Former-commit-id: 5ad04ec736fe184c258fb37854e3372e5d92ef01 [formerly efe119eaa8eeef0362b4c3ca7b6ab67b3c4e9a74]
    Former-commit-id: fbe41b280701a286588d615e33e635ac669ad387 [formerly 1fbe607ca0275a6c0c7a39052a325be119af381c]
    Former-commit-id: 624fc978a9639bf478c12b99cddb739e0bf227fa
    Former-commit-id: e0883493e7cdaa7ad2761e3b0b02190526cc1ece

--- test/entryServerStarter.js
+++ test/entryServerStarter.js
@@ -1,5 +1,7 @@
 var ES = require('../lib/entryServer');
-
+LOG.debug("Start ES")
 var startPort = parseInt(process.argv[2]);
+console.log("Create");
 var entryServer = new ES(startPort);
+console.log("Done")
 entryServer.init();
