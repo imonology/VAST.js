b9a754a437e9e67dada61a92ab2fef94b70a84a9
Miguel Smith <19287887@sun.ac.za>
2019-11-17 23:50:42 +0200

    Adjusted for Minecraft integration (not working)

--- test/vast_client3.bat
+++ test/vast_client3.bat
@@ -1,1 +1,1 @@
-node test_VAST_client.js true 192.168.43.101 37700 100 192.168.43.101 800 200 > C:\Users\judge\OneDrive\Documents\"University work"\Masters\Logs\clientMatch3.txt
+node test_VAST_client.js true 192.168.43.101 37700 100 192.168.43.101 800 200 > C:\Users\Miguel Smith\Documents\Varsity Work\Masters\Minecraft data parser\Logs\clientMatch3.txt
