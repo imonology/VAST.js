40f1d5e4af078dd043b7588ec551658883cedbf3
Miguel Smith <19287887@sun.ac.za>
2019-11-27 17:44:05 +0200

    Work in progress
    Former-commit-id: ce73f19af727b02334d0419c6a4cf15ebee640a8 [formerly d68cdc9a98019e4ca3b2caeeacde4b6414ac2f4f]
    Former-commit-id: cfdcd26bd17aaa1905090eff135bb72d5dab3a79 [formerly 4eb45669f6e2bfcbcdfd7138c95dfd04e945e4f5]
    Former-commit-id: 66f0546aea5c413a53acdb3f7ccb3da56c33b635
    Former-commit-id: 8942d12c0a61c58bdbfcb15f722bc1a04dac77b1

--- test/vast_client_gateway.bat
+++ test/vast_client_gateway.bat
@@ -1,1 +1,1 @@
-node test_VAST_client.js false 10.110.117.14 37700 100 10.110.117.14 200 200 > C:\Users\"Miguel Smith"\Documents\"Varsity Work"\Masters\"Minecraft data parser"\Logs\clientMatch0.txt
+node test_VAST_client.js false 10.110.117.14 37700 100 10.110.117.14 2000 2000 2 > C:\Users\"Miguel Smith"\Documents\"Varsity Work"\Masters\"Minecraft data parser"\Logs\clientMatch0.txt
