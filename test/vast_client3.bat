f5093d20064942ae82743fb43d7710f1e30635a3
Miguel Smith <19287887@sun.ac.za>
2019-11-27 18:25:23 +0200

    Fixed outstanding bugs
    Former-commit-id: 3415656322f652b80643ce483b2591f5546c7552 [formerly b5f8d89c6ff103bab35079f48269fa1a82d3cc3a]
    Former-commit-id: 69794f10e67ee8c074390bba43625d68ebe4fcc8 [formerly 5682f9b5eed144562a8818a289d8c23dbad917da]
    Former-commit-id: 702266865d1bf6707d3196a1a7aca24b8961b55a
    Former-commit-id: 347532e438fd1369a2f94d9131f279d94476008b

--- test/vast_client3.bat
+++ test/vast_client3.bat
@@ -1,1 +1,1 @@
-node test_VAST_client.js true 192.168.43.101 37700 100 192.168.43.101 800 200 > C:\Users\Miguel Smith\Documents\Varsity Work\Masters\Minecraft data parser\Logs\clientMatch3.txt
+node test_VAST_client.js true 10.110.117.14 37700 100 10.110.117.14 800 200 > C:\Users\Miguel Smith\Documents\Varsity Work\Masters\Minecraft data parser\Logs\clientMatch3.txt
