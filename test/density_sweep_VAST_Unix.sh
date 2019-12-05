48a0f05b8c5eb9f65d88732246ac7e1ea81d9e3f
Miguel Smith <19287887@sun.ac.za>
2019-12-06 00:24:21 +0200

    update
    Former-commit-id: 2b94536667f995b7007e384c357d417667a8bb11 [formerly d531d376d7147b45b3b4b08969c87a38c211725a]
    Former-commit-id: 36d7282704bc20c39f13052b5c6bd1b640287c52 [formerly 752510653b95fcab82078f68e3002488ab437e97]
    Former-commit-id: 3617ce3f5cca924567457a2b53533b545b1f1688
    Former-commit-id: 80c82c4b7da49546dc9de4f1fb922bf996ec5633

--- test/density_sweep_VAST_Unix.sh
+++ test/density_sweep_VAST_Unix.sh
@@ -6,14 +6,14 @@ GATEWAY=10.110.117.14
 CLIENTS=40
 #POSX=(0 700 500 700 300 500 500 100 900 300 700 500 500 400 600 400 600)
 #POSY=(0 700 500 300 700 100 900 500 500 500 500 300 700 400 400 600 600)
-CLIENT_THRESHOLD=(200 110 70 55 45 35 30 28 25 23 20 18 17 16 15 14 14)
-SUB_THRESHOLD=(220 120 80 60 50 40 35 33 30 27 24 22 21 20 19 18 18)
+CLIENT_THRESHOLD=(0 200 110 70 55 70 35 30 28 40 23 20 18 17 16 15 14 14)
+SUB_THRESHOLD=(0 220 120 80 60 80 40 35 33 30 50 24 22 21 20 19 18 18)
 
 for RADIUS in {16..16..16}; do
     	echo "Starting test for $RADIUS"
-	for CLIENTS in {50..500..50}; do
+	for CLIENTS in {300..300..50}; do
 		echo "Starting test for $CLIENTS"
-		for MATCHERS in {5..50..5}; do
+		for MATCHERS in {10..10..5}; do
 			echo "Starting test for $MATCHERS"
 
             echo "$TEST_SCRIPT/Movement/$RADIUS/$MATCHERS/$CLIENTS"
@@ -24,20 +24,20 @@ for RADIUS in {16..16..16}; do
             echo "Folder created. Starting entry server"
 
     		screen -S entryServer -d -m bash -c "./entryServerStarterUnix.sh 2999 > entryServer.txt"
-            sleep 1
+            sleep 0.5
 
             echo "Entry server screen created. Creating gateway"
 
-            screen -S VASTNode_0 -d -m bash -c "./vast_client_gateway_unix.sh $GATEWAY 37700 100 ${CLIENT_THRESHOLD[$MATCHERS]} ${SUB_THRESHOLD[$MATCHERS]} 0"
+            screen -S VASTNode_0 -d -m bash -c "./vast_client_gateway_unix.sh $GATEWAY 37700 100 undef undef ${CLIENT_THRESHOLD[$MATCHERS]} ${SUB_THRESHOLD[$MATCHERS]} 0"
             sleep 1
 
             echo "Gateway created. Creating matchers"
 
 			for ((c=1; c<=$((MATCHERS-1)); c++)) do
                 echo "Creating matcher $c"
-                echo "vast_client_unix.sh $GATEWAY 37700 100 undefined undefined ${CLIENT_THRESHOLD[$MATCHERS]} ${SUB_THRESHOLD[$MATCHERS]}"
-                screen -S VASTNode_"$c" -d -m bash -c "./vast_client_unix.sh $GATEWAY 37700 100 undefined undefined ${CLIENT_THRESHOLD[$MATCHERS]} ${SUB_THRESHOLD[$MATCHERS]} $c"
-                sleep 1
+                echo "vast_client_unix.sh $GATEWAY 37700 100 ${POSX[$c]} ${POSY[$c]} ${CLIENT_THRESHOLD[$MATCHERS]} ${SUB_THRESHOLD[$MATCHERS]}"
+                screen -S VASTNode_"$c" -d -m -L bash -c "./vast_client_unix.sh $GATEWAY 37700 100 undef undef ${CLIENT_THRESHOLD[$MATCHERS]} ${SUB_THRESHOLD[$MATCHERS]} $c"
+                sleep 2
             done
 
             echo "Matcher creation done. Connecting clients"
@@ -48,7 +48,8 @@ for RADIUS in {16..16..16}; do
                 echo "Clients per script: $client"
                 echo "test_client_unix.sh $client 1 $d 2999 $MATCHERS $CLIENTS $RADIUS"
                 screen -S Clients_"$d" -d -m bash -c " cd ../../../Pseudo\ MC\ client; ./test_client_unix.sh $client 1 $d 2999 $MATCHERS $CLIENTS $RADIUS"
-                sleeptime=$(echo "$client*0.2" | bc)
+                #sleeptime=$(echo "$client*0.2" | bc)
+                sleeptime=4
                 sleep $sleeptime
             done
 
