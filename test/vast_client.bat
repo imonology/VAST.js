57c2b61b6c4228ca3fca6dba3c48ae4a46993268
Miguel Smith <19287887@sun.ac.za>
2019-12-06 00:24:21 +0200

    update

--- test/vast_client.bat
+++ test/vast_client.bat
@@ -1,1 +1,1 @@
-node test_VAST_client.js true 10.110.117.14 37700 100 10.110.117.14 700 700 2 10 10> C:\Users\"Miguel Smith"\Documents\"Varsity Work"\Masters\"Minecraft data parser"\Logs\clientMatch1.txt
+node test_VAST_client.js true 10.110.117.14 37700 100 10.110.117.14 700 700 2 70 80> C:\Users\"Miguel Smith"\Documents\"Varsity Work"\Masters\"Minecraft data parser"\Logs\clientMatch1.txt
