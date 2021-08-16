21f4303f44788dac96fcf08da2cdc062036ab49e
cfmarais <charlfm1234@gmail.com>
2021-08-16 09:43:42 +0200

    Fixed small error in test script and decreased matcher AoI to test neighbour discovery.

--- test/client3_start.js
+++ test/client3_start.js
@@ -12,7 +12,7 @@ var C3 = new client('127.0.0.1', 10000, 510, 510, 1, function(){
 function publishMessages(){
     C3.publish(510, 500, 1, 'Client C published in centre', 1);
     C3.publish(485, 500, 1, 'Client C published on left', 1);
-    C3.publish(460, 500, 15, 'Client C published on left edge (centre outside, aoi intesects)', 1);
+    C3.publish(470, 500, 15, 'Client C published on left edge (centre outside, aoi intesects)', 1);
     C3.publish(550, 500, 15, 'Client C published on right edge (centre outside, aoi intesects)', 1);
     C3.publish(510, 525, 1, 'Client C published above', 1);
     C3.publish(620, 620, 15, 'Client C published at distant area', 1);
--- test/many_matchers.js
+++ test/many_matchers.js
@@ -4,47 +4,47 @@ var GW, L1, L2, L3, L4, R1, R2, R3, R4;
 var matcherCount = 0;
 
 function createGW(){
-    GW = new matcher(true, '127.0.0.1', 8000, 166.5, 166.5, 500, function(){
+    GW = new matcher(true, '127.0.0.1', 8000, 166.5, 166.5, 10, function(){
         matcherCount++
     });
 }
 function createM2(){
-    var M2 = new matcher(false, '127.0.0.1', 8000, 166.5, 832.5, 500, function(){
+    var M2 = new matcher(false, '127.0.0.1', 8000, 166.5, 832.5, 10, function(){
         matcherCount++
     });
 }
 function createM3(){
-    var M3 = new matcher(false, '127.0.0.1', 8000, 832.5, 832.5, 500, function(){
+    var M3 = new matcher(false, '127.0.0.1', 8000, 832.5, 832.5, 10, function(){
         matcherCount++
     });
 }
 function createM4(){
-    var M4 = new matcher(false, '127.0.0.1', 8000, 832.5, 166.5, 500, function(){
+    var M4 = new matcher(false, '127.0.0.1', 8000, 832.5, 166.5, 10, function(){
         matcherCount++
     });
 }
 function createM5(){
-    var M5 = new matcher(false, '127.0.0.1', 8000, 500, 500, 500, function(){
+    var M5 = new matcher(false, '127.0.0.1', 8000, 500, 500, 10, function(){
         matcherCount++
     });
 }
 function createM6(){
-    var M6 = new matcher(false, '127.0.0.1', 8000, 166.5, 500, 500, function(){
+    var M6 = new matcher(false, '127.0.0.1', 8000, 166.5, 500, 10, function(){
         matcherCount++
     });
 }
 function createM7(){
-    var M7 = new matcher(false, '127.0.0.1', 8000, 500, 832.5, 500, function(){
+    var M7 = new matcher(false, '127.0.0.1', 8000, 500, 832.5, 10, function(){
         matcherCount++
     });
 }
 function createM8(){
-    var M8 = new matcher(false, '127.0.0.1', 8000, 832.5, 500, 500, function(){
+    var M8 = new matcher(false, '127.0.0.1', 8000, 832.5, 500, 10, function(){
         matcherCount++
     });
 }
 function createM9(){
-    var M9 = new matcher(false, '127.0.0.1', 8000, 500, 166.5, 500, function(){
+    var M9 = new matcher(false, '127.0.0.1', 8000, 500, 166.5, 10, function(){
         matcherCount++
         console.log('Matchers are set up');
     });
@@ -58,44 +58,4 @@ setTimeout(createM6, 5000);
 setTimeout(createM7, 6000);
 setTimeout(createM8, 7000);
 setTimeout(createM9, 8000);
-
-/*
-// Callback hell, use promises or a timer in larger system
-
-// BL
-var GW = new matcher(true, '127.0.0.1', 8000, 166.5, 166.5, 500, function(){
-    mactherCount++;
-    // TL
-    var M2 = new matcher(false, '127.0.0.1', 8000, 166.5, 832.5, 500, function(){
-        mactherCount++;
-        //TR
-        var M3 = new matcher(false, '127.0.0.1', 8000, 832.5, 832.5, 500, function(){
-            mactherCount++;
-            // BR
-            var M4 = new matcher(false, '127.0.0.1', 8000, 832.5, 166.5, 500, function(){
-                mactherCount++;
-                // C
-                var M5 = new matcher(false, '127.0.0.1', 8000, 500, 500, 500, function(){
-                    mactherCount++;
-                    // CL
-                    var M6 = new matcher(false, '127.0.0.1', 8000, 166.5, 500, 500, function(){
-                        mactherCount++;
-                        // TC
-                        var M7 = new matcher(false, '127.0.0.1', 8000, 500, 832.5, 500, function(){
-                            mactherCount++;
-                            // CR
-                            var M8 = new matcher(false, '127.0.0.1', 8000, 832.5, 500, 500, function(){
-                                mactherCount++;
-                                // BC
-                                var M9 = new matcher(false, '127.0.0.1', 8000, 500, 166.5, 500, function(){
-                                });
-                            });
-                        });
-                    });
-                });
-            });
-        });
-    });
-});
-*/
 
