6fe966c91f3d2979addce1c2ee22d42cf6d16df4
cfmarais <charlfm1234@gmail.com>
2021-08-18 16:15:49 +0200

    no message

--- test/client1_start.js
+++ test/client1_start.js
@@ -8,14 +8,13 @@ console.log('I am Client A');
 var C1 = new client('127.0.0.1', 10000, 500, 500, 1,
     // When assigned to matcher, callback
     function(){
-        C1.subscribe(500, 500, 20, 1);
+        C1.subscribe(660, 660, 50, 1);
     });
 
 function publishMessages(){
-    C1.publish(510, 500, 1, 'Client A published in centre', 1);
-    C1.publish(485, 500, 1, 'Client A published on left', 1);
+
 }
 
 // give time for clients to join and subscribe first
-setTimeout(publishMessages, 15000);
+setTimeout(publishMessages, 5000);
 
