faf9f93b366b74c961c0833e799dd3f4296dfe70
cfmarais <charlfm1234@gmail.com>
2021-08-29 19:56:31 +0200

    -Overlapping Subscriptions -Distant Subscriptions -Distant and Overlapping Subscriptions -First-order sub forwarding optimisation (no secondary neighbour discovery yet)
    Former-commit-id: 1ec90f3e1fc9593159c8c947f5bf3cdd655ed7ef

--- test/client1_start.js
+++ test/client1_start.js
@@ -1,18 +1,16 @@
 const client = require('../lib/client');
 
-// Testing Spatial Pub / Sub between 3 clients. Client A on left, Client B on right, Client C covers all
-// Client B is also subbed to small area at [620, 620]
-
 console.log('I am Client A');
 
-var C1 = new client('127.0.0.1', 10000, 500, 500, 1,
+var C1 = new client('127.0.0.1', 20000, 500, 500, 1,
     // When assigned to matcher, callback
     function(){
-        C1.subscribe(660, 660, 50, 1);
+        console.log('Subscribing to overlap with top-right')
+//        C1.subscribe(650, 650, 50, 1);
     });
 
 function publishMessages(){
-
+    C1.publish(500, 350, 75, 'Client A publishing at center-bottom overlap', 1);
 }
 
 // give time for clients to join and subscribe first
--- test/client2_start.js
+++ test/client2_start.js
@@ -1,20 +1,15 @@
 const client = require('../lib/client');
 
-// Testing Spatial Pub / Sub between 3 clients. Client A on left, Client B on right, Client C covers all
-// Client B is also subbed to small area at [620, 620]
-
 console.log('I am Client B');
 
-var C2 = new client('127.0.0.1', 10000, 520, 520, 1, function(){
-    C2.subscribe(520, 500, 20, 1);
-    C2.subscribe(620, 620, 10, 1); // test if multiple subs works
+var C2 = new client('127.0.0.1', 20000, 800, 800, 1, function(){
+    console.log('Subscribing at center-bottom. Far away and overlapping');
+    C2.subscribe(500, 300, 75, 1);    
 });
 
 
 function publishMessages(){
-    C2.publish(510, 500, 1, 'Client B published in centre', 1);
-    C2.publish(535, 500, 1, 'Client B published on right', 1);
-    C2.publish(600, 600, 20, 'Client B publishing at distant area');
+//    C2.publish(680, 680,10,'Client B published at top-right, near themself', 1)
 }
 
 // give time for clients to join and subscribe first
