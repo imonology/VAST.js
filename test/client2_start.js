4c6c110dff1329ac5f1a6c9f1ede941b2f1b34d1
cfmarais <charlfm1234@gmail.com>
2021-09-05 18:00:53 +0200

    -Spatial message routing fully implemented -Complete seperatation between matcher and VON peer for message forwarding -'VoroCast' message forwarding implemented for all area messages -Duplicate Pubs issue solved for common overlapping regions between pub and matching sub (a bit untidy)

--- test/client2_start.js
+++ test/client2_start.js
@@ -2,14 +2,14 @@ const client = require('../lib/client');
 
 console.log('I am Client B');
 
-var C2 = new client('127.0.0.1', 20000, 800, 800, 1, function(){
+var C2 = new client('127.0.0.1', 20000, 800, 500, 1, function(){
     console.log('Subscribing at center-bottom. Far away and overlapping');
     C2.subscribe(500, 300, 75, 1);    
 });
 
 
 function publishMessages(){
-    C2.publish(680, 680,10,'Client B published at top-right, near themself', 1)
+    C2.publish(680, 680, 50,'Client B published at top-right, near themself', 1)
 }
 
 // give time for clients to join and subscribe first
