193ddf23dc2fc71922c7b095ae74b15201fc288c
cfmarais <charlfm1234@gmail.com>
2021-09-05 18:00:53 +0200

    -Spatial message routing fully implemented -Complete seperatation between matcher and VON peer for message forwarding -'VoroCast' message forwarding implemented for all area messages -Duplicate Pubs issue solved for common overlapping regions between pub and matching sub (a bit untidy)
    Former-commit-id: 17914736b5f7282f8d7507d5e16b8ad1b2c51226

--- test/many_matchers.js
+++ test/many_matchers.js
@@ -3,58 +3,58 @@ const matcher = require('../lib/matcher.js');
 var matcherCount = 0;
 
 function createGW(){
-    var GW = new matcher(true, '127.0.0.1', 8000, 166.5, 832.5, 10, function(){
+    var GW = new matcher(true, '127.0.0.1', 8000, 166.5, 832.5, 400, function(){
         matcherCount++;
     });
 }
 function createM2(){
-    var M2 = new matcher(false, '127.0.0.1', 8000, 500, 832.5, 10, function(){
+    var M2 = new matcher(false, '127.0.0.1', 8000, 500, 832.5, 400, function(){
         matcherCount++;
     });
 }
 function createM3(){
-    var M3 = new matcher(false, '127.0.0.1', 8000, 832.5, 832.5, 10, function(){
+    var M3 = new matcher(false, '127.0.0.1', 8000, 832.5, 832.5, 400, function(){
         matcherCount++;
     });
 }
 function createM4(){
-    var M4 = new matcher(false, '127.0.0.1', 8000, 166.5, 500, 10, function(){
+    var M4 = new matcher(false, '127.0.0.1', 8000, 166.5, 500, 400, function(){
         matcherCount++;
     });
 }
 function createM5(){
-    var M5 = new matcher(false, '127.0.0.1', 8000, 500, 500, 10, function(){
+    var M5 = new matcher(false, '127.0.0.1', 8000, 500, 500, 400, function(){
         matcherCount++;
     });
 }
 function createM6(){
-    var M6 = new matcher(false, '127.0.0.1', 8000, 832.5, 500, 10, function(){
+    var M6 = new matcher(false, '127.0.0.1', 8000, 832.5, 500, 400, function(){
         matcherCount++;
     });
 }
 function createM7(){
-    var M7 = new matcher(false, '127.0.0.1', 8000, 166.5, 166.5, 10, function(){
+    var M7 = new matcher(false, '127.0.0.1', 8000, 166.5, 166.5, 400, function(){
         matcherCount++;
     });
 }
 function createM8(){
-    var M8 = new matcher(false, '127.0.0.1', 8000, 500, 166.5, 10, function(){
+    var M8 = new matcher(false, '127.0.0.1', 8000, 500, 166.5, 400, function(){
         matcherCount++;
     });
 }
 function createM9(){
-    var M9 = new matcher(false, '127.0.0.1', 8000, 832.5, 166.5, 10, function(){
+    var M9 = new matcher(false, '127.0.0.1', 8000, 832.5, 166.5, 400, function(){
         matcherCount++;
         console.log('Matchers are set up');
     });
 }
 createGW();
-setTimeout(createM2, 2000);
-setTimeout(createM3, 3000);
-setTimeout(createM4, 4000);
-setTimeout(createM5, 5000);
-setTimeout(createM6, 6000);
-setTimeout(createM7, 7000);
-setTimeout(createM8, 8000);
-setTimeout(createM9, 9000);
+setTimeout(createM2, 200);
+setTimeout(createM3, 300);
+setTimeout(createM4, 400);
+setTimeout(createM5, 500);
+setTimeout(createM6, 600);
+setTimeout(createM7, 700);
+setTimeout(createM8, 800);
+setTimeout(createM9, 900);
 
