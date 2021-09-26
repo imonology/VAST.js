c81f12727b8c381ebe60477ee3d2eeaab34e0948
cfmarais <charlfm1234@gmail.com>
2021-09-26 23:01:58 +0200

    -better visualiser, implemented localised views and subscription drawing

--- test/client2_start.js
+++ test/client2_start.js
@@ -4,12 +4,13 @@ console.log('I am Client B');
 
 var C2 = new client('127.0.0.1', 20000, 800, 500, 1, function(){
     console.log('Subscribing at center-bottom. Far away and overlapping');
-    C2.subscribe(500, 300, 75, 1);    
+   // C2.subscribe(500, 300, 75, 1);    
 });
 
 
 function publishMessages(){
-    C2.publish(680, 680, 50,'Client B published at top-right, near themself', 1)
+    //C2.publish(680, 680, 50,'Client B published at top-right, near themself', 1)
+    C2.publish(240, 750, 350,'Client B published at corner, huge radius', 1)
 }
 
 // give time for clients to join and subscribe first
