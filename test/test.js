95c8beb3a7bb582f669d59e58aa181a5c6322c7a
cfmarais <charlfm1234@gmail.com>
2021-10-13 13:12:27 +0200

    no message

--- test/test.js
+++ test/test.js
@@ -1,7 +1,26 @@
 require('../lib/common.js');
 
-UTIL.getLocalIP(function(ip){
-    console.log(ip);
-})
+//var networkInterfaces = require('os').networkInterfaces();
+//var address = networkInterfaces['eth0'][0]['address'];
+
+function getIPAddress() {
+    var interfaces = require('os').networkInterfaces();
+    console.log(interfaces);
+    for (var devName in interfaces) {
+      var iface = interfaces[devName];
+  
+      for (var i = 0; i < iface.length; i++) {
+        var alias = iface[i];
+        if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+          return alias.address;
+      }
+    }
+    return '0.0.0.0';
+}
+
+address = getIPAddress();
+
+
+console.log(address);
 
 
