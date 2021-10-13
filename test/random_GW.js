df5d6ba13a32557007306bf2ef30119e737c5198
cfmarais <charlfm1234@gmail.com>
2021-10-13 10:08:02 +0200

    no message

--- test/random_GW.js
+++ test/random_GW.js
@@ -3,4 +3,6 @@ const matcher = require('../lib/matcher.js');
 var x = Math.random()*1000;
 var y = Math.random()*1000;
 
-var GW = new matcher(true, '127.0.0.1', 8000, x, y, 100, function(){});
+var GW = new matcher(true, '0.0.0.0', 8000, x, y, 100, function(id){
+    console.log('I have joined as gateway with ID: '+id);
+});
