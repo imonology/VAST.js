56d213feedfa4e12a23ea935dae4b5aa57eeac79
cfmarais <charlfm1234@gmail.com>
2021-10-13 12:08:58 +0200

    no message

--- test/test.js
+++ test/test.js
@@ -1,67 +1,7 @@
-const { json } = require("express");
+require('../lib/common.js');
 
-var obj1 = {
-    arr : {
-        0 : 31,
-        1 : 21
-    },
-
-    arr2 : {
-        pos1 : {
-            x : 10,
-            y : 15
-        },
-        pos2 : {
-            x : 11,
-            y : 11
-        }
-    },
-
-    id : 1
-}
-
-var obj2 = {
-    arr : {
-        0 : 31,
-        1 : 21
-    },
-
-    arr2 : {
-        pos1 : {
-            x : 10,
-            y : 15
-        },
-        pos2 : {
-            x : 11,
-            y : 11
-        }
-    },
-    
-    id : 2
-}
-
-var obj3 = {
-    arr : {
-        0 : 32,
-        1 : 21
-    },
-
-    arr2 : {
-        pos1 : {
-            x : 10,
-            y : 15
-        },
-        pos2 : {
-            x : 11,
-            y : 11
-        }
-    },
-    
-    id : 1
-}
-
-var one = JSON.stringify(obj1) === JSON.stringify(obj2);
-var two = JSON.stringify(obj1) === JSON.stringify(obj3);
-console.log(one, two);
+UTIL.getLocalIP(function(ip){
+    console.log(ip);
+})
 
 
