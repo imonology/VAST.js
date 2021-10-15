f9f26c58d8c9541e74a16e7e3ad4ce3bfb3366d1
cfmarais <charlfm1234@gmail.com>
2021-10-16 01:03:03 +0200

    Added latency and bandwidth calcs for client spatial PING/PONG Implemented file write for the results Optmised Area-Forwarding in VON_peer Client names now contain hostname for easier identification
    Former-commit-id: 71f4d501aa2ffde4c0fbe6cf11bdfb27f0c5ed03

--- test/new_client.js
+++ test/new_client.js
@@ -5,7 +5,7 @@ var sub_x = process.argv[2] || 500;
 var sub_y = process.argv[3] || 500;
 var sub_radius = process.argv[4] || 300;
 var wait_to_ping = process.argv[5] || 1000;
-var ping_refresh_time = process.argv[6] || 5000;
+var ping_refresh_time = process.argv[6] || 1000;
 var wait_to_unsubscribe = process.argv[7] || 0;
 
 var x = process.argv[8] || Math.random()*1000;
@@ -24,7 +24,7 @@ UTIL.lookupIP('LAPTOP-JJ5440PB', function(addr){
 
     C = new client(GW_addr, 20000, x, y, 1, function(id){
         console.log('Client: ' + id + ' subscribing around themself at {x: '+x+'; y: '+y+'; radius: '+aoi_radius+'}');
-        //C.subscribe(x, y, aoi_radius, 1);
+        C.subscribe(x, y, aoi_radius, 2);
     
         console.log('Client: ' + id + ' subscribing at {x: '+sub_x+'; y: '+sub_y+'; radius: '+sub_radius+'}');
         C.subscribe(sub_x, sub_y, sub_radius, 1);
@@ -39,9 +39,9 @@ function publishMessage(){
 
     //C.publish(x, y, 1,'Client: '  + _id + ' published with small radius around themself', 1);
 
-    C.publish(sub_x, sub_y, sub_radius,'Client: '  + _id + ' published with equal radius', 1);
+    //C.publish(sub_x, sub_y, sub_radius,'Client: '  + _id + ' published with equal radius', 1);
     //C.publish(sub_x, sub_y, 1,'Client: '  + _id + ' published at center of subsciption', 1);
-    //C.publish(1, 1, 1000, 'Client: '  + _id + ' published at corner with huge radius', 1)
+    C.publish(1, 1, 1000, 'Client: '  + _id + ' published at corner with huge radius', 1)
 
     //setTimeout(publishMessage, pub_refresh_time);
 }
@@ -49,8 +49,9 @@ function publishMessage(){
 function sendPINGs(){
     console.log('I am pinging');
 
-    console.log('sending ping to entire region');
-    C.sendPING(500, 500, 2000, 1);
+    setInterval(function(){
+        C.sendPING(500, 500, 2000, 1);
+    }, ping_refresh_time)
 }
 
 function clearSubscriptions(){
@@ -59,7 +60,7 @@ function clearSubscriptions(){
 }
 
 // give time for clients to join and subscribe first
-//setTimeout(sendPINGs, wait_to_ping);
+setTimeout(sendPINGs, wait_to_ping);
 
 if (wait_to_unsubscribe > 1000){
     //setTimeout(clearSubscriptions, wait_to_unsubscribe);
