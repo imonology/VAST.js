09af4774ab3c8ce4419abe82fa69ee6d6329322d
cfmarais <charlfm1234@gmail.com>
2021-10-16 11:49:39 +0200

    Fixed duplicate pub issue Optimised matched publication forwarding (slightly)

--- test/start_clients.bat
+++ test/start_clients.bat
@@ -1,5 +1,5 @@
-start node new_client.js 500 500 100 10000 100 0
-start node new_client.js 500 500 100 10000 100 0
-start node new_client.js 500 500 100 10000 100 0
-start node new_client.js 500 500 100 10000 100 0
-start node new_client.js 500 500 100 10000 100 0
+start node new_client.js 500 500 500 1000 5000 0
+start node new_client.js 500 500 500 1000 5000 0
+start node new_client.js 500 500 500 1000 5000 0
+start node new_client.js 500 500 500 1000 5000 0
+start node new_client.js 500 500 500 1000 5000 0
