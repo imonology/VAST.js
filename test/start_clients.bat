bd74b9e5565d492753761abb2b598e806209335c
cfmarais <charlfm1234@gmail.com>
2021-10-17 11:04:55 +0200

    - Matcher and VON now run on seperate threads

--- test/start_clients.bat
+++ test/start_clients.bat
@@ -1,5 +1,1 @@
 start node new_client.js 500 500 500 1000 5000 0
-start node new_client.js 500 500 500 1000 5000 0
-start node new_client.js 500 500 500 1000 5000 0
-start node new_client.js 500 500 500 1000 5000 0
-start node new_client.js 500 500 500 1000 5000 0
