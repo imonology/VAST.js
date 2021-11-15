90a5521a94144704dc09dd7833847078066c92be
Debojyoti1915001 <debojyoti76@gmail.com>
2021-11-15 21:15:39 +0530

    added env file for computer name
    Former-commit-id: 553a0b0810e6bc4eb63844cac3b566552c00063d

--- test/start_matcher.js
+++ test/start_matcher.js
@@ -5,9 +5,9 @@ var y = parseFloat(process.argv[3]) || Math.random()*1000;
 var radius = parseFloat(process.argv[4]) || 100;
 
 var M;
-
 // find gateway address before instantiating matcher
-UTIL.lookupIP('localhost', function(addr){
+console.log(process.env.COMPUTER_NAME)
+UTIL.lookupIP(process.env.COMPUTER_NAME, function(addr){
 
     M = new matcher(false, addr, 8000, x, y, radius, function(id){
         console.log('I have joined with id: ' + id);
