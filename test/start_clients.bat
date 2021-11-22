4d806296e2c6ba6ae6ab9dbafb08e8615f906ae8
Victory <opeolu.victory@gmail.com>
2021-11-22 15:45:10 +0200

    removed old test_VAST_IO & GW, log files
    Former-commit-id: 6c85f862d69d3187c668e0b329d123016d7402c0

--- test/start_clients.bat
+++ test/start_clients.bat
@@ -1,2 +1,5 @@
-start node new_client.js 100 100 100 200 200 200 true
+start node new_client.js (100 100 100) 200 200 200 true
 start node new_client.js 100 100 100 200 200 200 false
+
+
+@REM start node new_client.js aoi=(x=10, y=15, r=100), RequestType=(pub)
