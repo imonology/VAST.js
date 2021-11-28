82a2fc9188de067db317d360baea2e4b3b1bb28a
Opeolu <Victory.Opeolu@vcontractor.co.za>
2021-11-28 11:03:34 +0200

    added publish in 5 iterations
    Former-commit-id: e876e515a11081d532ba9a7f3fc0c8cdc33ba4f1 [formerly 13bb6c6881470b41a921e9b7c4f716400325112c]
    Former-commit-id: ebf907ef1f22147370b8f590813651e77918424d

--- test/start_clients.bat
+++ test/start_clients.bat
@@ -1,4 +1,4 @@
-start node new_client.js (100 100 100) 200 200 200 true
+start node new_client.js 100 100 100200 200 200 true
 start node new_client.js 100 100 100 200 200 200 false
 
 
--- test/test.js
+++ test/test.js
@@ -13,3 +13,8 @@ var timestamp = (hrTime[0] * 1000000 + hrTime[1] / 1000) //current time in micro
 console.log(timestamp)
 //onsole.log(crypto.randomBytes(10));
 //console.log(JSON.stringify(crypto.randomBytes(10)));
+
+
+// for (var i = 0; i < 5; i++) {
+//     console.log("Hi");
+//   }
