6b63f3e10fd7028fdf52375183fd10a94d7ec300
Opeolu <Victory.Opeolu@vcontractor.co.za>
2021-11-30 15:11:39 +0200

    cleaning up text logger

--- test/test.js
+++ test/test.js
@@ -16,8 +16,11 @@ console.log(timestamp)
 
 
 // for (var i = 0; i < 5; i++) {
-//     console.log("Hi");
-//   }
+//     setTimeout(function timer() {
+//         console.log("hello world");
+//       }, i * 3000);
+//     //  console.log("Hi");
+//    }
 
 // Returns a random integer from 0 to 9:
 random = Math.floor(Math.random() * 26);
@@ -25,3 +28,48 @@ console.log("random is : ", random);
 
 var random2 = Math.floor(Math.random() * (50 - 26 + 1)) + 26;
 console.log("random is : ", random2);
+
+const csv = require('csv-parser')
+const fs = require('fs')
+const logger = require("../lib/common/logger")
+const results = [];
+
+// require('fs');
+
+// fs.writeFile('hello.txt', 'Hello World!', function (err) {
+// //   if (err) return console.log(err);
+
+//   console.log('Hello World > helloworld.txt');
+// });
+
+
+
+//  for (var i = 0; i < 5; i++) {
+
+//      setTimeout(function timer() {
+//         //  console.log(timestamp, "hello world");
+//         //  var A = Date.now();
+//         var hrTime = process.hrtime()
+//         var timestamp = (hrTime[0] * 1000000 + hrTime[1] / 1000) //current time in microseconds
+//          console.log(timestamp)
+
+//        }, i * 1000);
+//      //  console.log("Hi");
+//     }
+
+
+// // var logger = require('../lib/common/logger')
+var lib = require("../lib/common/logging.js");
+var N;
+// // console.log('prelog is ', log_one.LogToFile)
+// log_oneLogToFile('test.txt', 'this is a test');
+// // console.log('postlog is ', log_one.LogToFile)
+
+// LogTo
+
+// result = lib.say('check')
+// console.log('The result is: ', result);
+
+result = lib.LogToFile('test.txt', 'this is a test')
+
+// var mycar = new logging.Car('Eagle', 'Talon TSi', 1993);
--- test/test_client.js
+++ test/test_client.js
@@ -1,12 +1,17 @@
 const client = require("../lib/client");
 require("../lib/common.js");
+
+require('../lib/common/logger.js'); //for logging
+
+var lib = require("../lib/common/logging.js");
+
 require("dotenv").config();
+
 const SIZE = 1000; // world size
 const fs = require("fs");
 const csv = require("csv-parser");
 
-var hrTime = process.hrtime()
-var timestamp = (hrTime[0] * 1000000 + hrTime[1] / 1000) //current time in microseconds
+
 
 // my position and AoI to subscribe for PONG messages
 var x = process.argv[2] || Math.random() * SIZE;
@@ -17,7 +22,57 @@ var type = process.argv[5];
 // console.log(type)
 
 var channel = 'Channel'
+var payload = 'PING'
 
+
+
+// CSV functions to write and read
+
+// const data = [];
+// function writeToCSVFile() {
+//   fs.createReadStream("./output.csv")
+//     .pipe(csv())
+//     .on("data", function (row) {
+//       // console.log(row);
+//       const _data = {
+//           timestamp: row.timestamp,
+//           x: row.x,
+//           y: row.y,
+//           r: row.r,
+//           type: row.type,
+//           payload: row.payload
+//       };
+//       data.push(_data);
+      
+//     })
+//     .on("end", function () {
+//       console.table(data);
+//       // TODO: SAVE users data to another file
+//     });
+//   data.push({timestamp, x, y, r, type, payload});
+
+//   const filename = "output.csv";
+  
+//   fs.writeFile(filename, extractAsCSV(), (err) => {
+//     if (err) {
+//       console.log("Error writing to csv file", err);
+//     } else {
+//       console.log(`saved as ${filename}`);
+//     }
+//   });
+//   // return writeToCSVFile;
+// }
+
+// function extractAsCSV() {
+//   const header = ["time, x, y, r, RequestType, Payload"];
+
+//   const rows = data.map((d) => `${d.timestamp}, ${d.x}, ${d.y}, ${d.r}, ${d.type}, ${d.payload}`);
+//   return header.concat(rows).join("\n");
+// }
+
+// // ------ 
+
+// writeToCSVFile();
 
 // if (type == "subscribe") {
 //   console.log('true')
@@ -51,52 +106,29 @@ if (type == "subscribe") {
   
     UTIL.lookupIP("127.0.0.1", function (addr) {
     GW_addr = addr;
-    const data = [];
-
-    function writeToCSVFile() {
-      fs.createReadStream("./output.csv")
-        .pipe(csv())
-        .on("data", function (row) {
-          console.log(row);
-          const _data = {
-              timestamp: row.timestamp,
-              x: row.x,
-              y: row.y,
-              r: row.r,
-              type: row.type,
-          };
-          data.push(_data);
-        })
-        .on("end", function () {
-          console.table(data);
-          // TODO: SAVE users data to another file
-        });
-      data.push({timestamp, x, y, r, type});
-      const filename = "output.csv";
-      fs.writeFile(filename, extractAsCSV(), (err) => {
-        if (err) {
-          console.log("Error writing to csv file", err);
-        } else {
-          console.log(`saved as ${filename}`);
-        }
-      });
-    }
-
-    function extractAsCSV() {
-      const header = ["time, x, y, r, RequestType"];
-
-      const rows = data.map((d) => `${d.timestamp}, ${d.x}, ${d.y}, ${d.r}, ${d.type}, ${d.channel}`);
-      return header.concat(rows).join("\n");
-    }
-    
-    writeToCSVFtaskile();
-
+        
     C = new client(GW_addr, 20000, x, y, r, function (id) {
       _id = id;
+      
+      var pub_data_header = ('timestamp, x, y, r \n')
+      result = lib.LogToFile('test.txt', pub_data_header)
+      // var pub_data = ('\n '+timestamp+', '+x+', '+y+', '+r+'')
 
       for (var i = 0; i < 5; i++) {
-        C.publish(x, y, r, "100", channel);
-      }
+        setTimeout(function timer() {
+          var timestamp = Date.now();
+          C.publish(x, y, r, "10", channel)
+          pub_data = (''+timestamp+', '+x+', '+y+', '+r+'');
+          // B = new LogToFile(process.env.CLIENT_FILENAME, pub_data);
+          // console.log(B)
+          console.log(pub_data)
+
+          result = lib.LogToFile('test.txt', pub_data)
+          // console.log(_id + ' is publishing to area :{x: '+x+'; y: '+y+'; radius: '+r+'}');
+        }, i * 1000);
+        
+      //  console.log("Hi");
+     }
 
     });
   });
