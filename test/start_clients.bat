2d674be5ed55067bf5a0ee4c1152be4405fe5781
cfmarais <charlfm1234@gmail.com>
2022-01-26 07:50:19 +0200

    no message
    Former-commit-id: 87b29fdbfbea2989a8deb00c1848f46d3046763f

--- test/start_clients.bat
+++ test/start_clients.bat
@@ -1,2 +1,3 @@
-start node new_client.js 100 100 100 200 200 200 true
-start node new_client.js 100 100 100 200 200 200 false
+start node new_client.js 100 100 10 100 100 2000 false
+start node new_client.js 900 900 10 900 900 2000 false
+start node new_client.js 100 900 10 100 900 2000 true
